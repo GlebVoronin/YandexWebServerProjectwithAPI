dfdd1aee8c4112ca5f5947b752ba84459219caee
GlebVoronin <47082791+GlebVoronin@users.noreply.github.com>
2020-05-08 01:25:07 +0300

    Update api test

---
+++ api_tests/test_cloth_api.py
@@ -0,0 +1,47 @@
+from requests import get, post, put, delete
+
+cloth_api_server = 'http://cloths-shop-prorotype.herokuapp.com/api/cloths'
+# 'id', 'title', 'description', 'images_links', 'colors',
+# 'length', 'price', 'date', 'country_id' - возможные поля, используемые в бд
+"""Для выполнения корректного запроса требуется заполнение всех полей модели кроме id!"""
+"""Корректные"""
+"""ClothListResource"""
+print(get(cloth_api_server).json())
+images_links = '/static/img/cloth/image_23;/static/img/cloth/image_24;' \
+               '/static/img/cloth/image_25;/static/img/cloth/image_26'
+print(post(cloth_api_server, json={'title': 'Ткань_2', 'description': 'Описания\nнет',
+                                   'images_links': images_links, 'colors': 'синий',
+                                   'length': 120.0, 'price': 475,
+                                   'date': 'None', 'country_id': 1}).json())
+print(get(cloth_api_server).json())  # проверка добавления
+"""ClothResource"""
+print(get(cloth_api_server).json())  # проверка исходных значений
+print(get(cloth_api_server + '/2').json())  # получение избранного по id
+# изменение по id
+print(put(cloth_api_server + '/1', json={'title': 'Ткань_2', 'description': 'Описания\nнет',
+                                         'images_links': images_links, 'colors': 'синий',
+                                         'length': 247.0, 'price': 475,
+                                         'date': 'None', 'country_id': 1}).json())
+print(get(cloth_api_server + '/2').json())  # подтверждение изменения
+print(delete(cloth_api_server + '/2').json())  # удаление по id
+print(delete(cloth_api_server + '/2').json())  # подтверждение удаления
+print(get(cloth_api_server).json())  # проверка результатов
+"""Некорректные"""
+"""ClothListResource"""
+# лишний слэш (/) API считает за пустой аргумент id -> неверно
+print(get('http://cloths-shop-prorotype.herokuapp.com/api/cloths/').json())
+print(post(cloth_api_server, json={}).json())  # пустой запрос
+# country_id не заполнено
+print(post(cloth_api_server, json={'title': 'Ткань_2', 'description': 'Описания\nнет',
+                                   'images_links': images_links, 'colors': 'синий',
+                                   'length': 120.0, 'price': 475,
+                                   'date': 'None'}).json())
+"""ClothResource"""
+print(get(cloth_api_server + "/abc").json())  # неверный id т.к. не int
+# предположительно некорректный запрос. Вероятнее всего, такого id нет
+print(get(cloth_api_server + "/787451188").json())
+print(delete(cloth_api_server + "/787451188").json())  # то же, что и впредыдущем пункте
+print(delete(cloth_api_server + "/abc").json())  # такого id нет
+print(put(cloth_api_server + '/2', json={}))  # пустой запрос
+# запрос без title и других параметров, set - параметра напротив в api/cloths
+print(put(cloth_api_server + '/2', json={'set': ''}))

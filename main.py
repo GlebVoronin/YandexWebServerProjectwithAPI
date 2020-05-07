5f5679cb81aee3217fcf6783724e0015bdd9258e
GlebVoronin <47082791+GlebVoronin@users.noreply.github.com>
2020-05-08 02:29:52 +0300

    Обновление просмотра изображений

--- main.py
+++ main.py
@@ -457,5 +457,4 @@ def not_found(error):
 
 
 if __name__ == '__main__':
-    app.run()
-    # app.run(host='0.0.0.0', port=os.environ.get('PORT', 33507))
+    app.run(host='0.0.0.0', port=os.environ.get('PORT', 33507))

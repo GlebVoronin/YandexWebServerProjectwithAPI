d8bdc1f01ec069c4dbf76b642f5ab762e9d796cc
GlebVoronin <47082791+GlebVoronin@users.noreply.github.com>
2020-05-07 22:08:28 +0300

    Adding API tests

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ from data.models.countries import Country
 from data.models.orders import Order
 from data.models.favourites import FavouriteItems
 from data.forms import RegisterForm, LoginForm, AddClothForm, OrderForm, OrderRegistrationForm
-from flask_ngrok import run_with_ngrok
 from flask_login import (login_user, logout_user, login_required, LoginManager, current_user)
 from flask_restful import Api
 from resources import all_resources
@@ -22,7 +21,6 @@ ADMINISTRATOR_PASSWORD = 'r651I45H5P3Za45s'
 COUNT_OF_CLOTHS_BY_ONE_PAGE = 30
 DB_NAME = 'Main'
 app = Flask(__name__)
-run_with_ngrok(app)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 db_session.global_init(f'db/{DB_NAME}.sqlite')
 app.config['PERMANENT_SESSION_LIFETIME'] = datetime.timedelta(days=365)
@@ -449,4 +447,5 @@ def not_found(error):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=os.environ.get('PORT', 33507))
+    # app.run(host='0.0.0.0', port=os.environ.get('PORT', 33507)) for heroku
+    app.run()

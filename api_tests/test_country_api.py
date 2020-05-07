c5430b340e55e063396271656d8cf8ecdc32409f
GlebVoronin <47082791+GlebVoronin@users.noreply.github.com>
2020-05-07 23:19:52 +0300

    New comments, bug fix, api-test for countries

--- api_tests/test_country_api.py
+++ api_tests/test_country_api.py
@@ -8,13 +8,13 @@ country_api_server = 'http://localhost:5000/api/countries'
 """
 """CountryListResource"""
 print(get(country_api_server).json())
-print(post(country_api_server, json={'id': 1, 'title': 'Италия'}).json())
+print(post(country_api_server, json={'id': 2, 'title': 'Италия'}).json())
 print(get(country_api_server).json())  # проверка добавления
 """CountryResource"""
 print(get(country_api_server).json())  # проверка исходных значений
-print(get(country_api_server + '/1').json())  # получение страны по id
-print(put(country_api_server + '/1', json={'id': 1, 'title': 'США'}).json())  # изменение по id
-print(get(country_api_server + '/1').json())  # подтверждение изменения
-print(delete(country_api_server + '/1').json())  # удаление по id
-print(delete(country_api_server + '/1').json())  # подтверждение удаления
+print(get(country_api_server + '/2').json())  # получение страны по id
+print(put(country_api_server + '/2', json={'id': 2, 'title': 'США'}).json())  # изменение по id
+print(get(country_api_server + '/2').json())  # подтверждение изменения
+print(delete(country_api_server + '/2').json())  # удаление по id
+print(delete(country_api_server + '/2').json())  # подтверждение удаления
 print(get(country_api_server).json())  # проверка результатов

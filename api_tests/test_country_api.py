3b76e5914f5927e2a3cf8198f6d7e98a9a42f67d
GlebVoronin <47082791+GlebVoronin@users.noreply.github.com>
2020-05-08 00:10:34 +0300

    Update api tests

--- api_tests/test_country_api.py
+++ api_tests/test_country_api.py
@@ -1,20 +1,25 @@
 from requests import get, post, put, delete
 
-country_api_server = 'http://localhost:5000/api/countries'
-# 'id', 'title' - возможные поля, используемые в бд
-"""Для выполнения корректного запроса требуется заполнение всех полей модели !"""
-"""
-Корректные
-"""
+country_api_server = 'http://cloths-shop-prorotype.herokuapp.com/api/countries'
+# # 'id', 'title' - возможные поля, используемые в бд
+# """Для выполнения корректного запроса требуется заполнение всех полей модели !"""
+# """Корректные"""
+# """CountryListResource"""
+# print(get(country_api_server).json())
+# print(post(country_api_server, json={'id': 2, 'title': 'Италия'}).json())
+# print(get(country_api_server).json())  # проверка добавления
+# """CountryResource"""
+# print(get(country_api_server).json())  # проверка исходных значений
+# print(get(country_api_server + '/2').json())  # получение страны по id
+# print(put(country_api_server + '/2', json={'id': 2, 'title': 'США'}).json())  # изменение по id
+# print(get(country_api_server + '/2').json())  # подтверждение изменения
+# print(delete(country_api_server + '/2').json())  # удаление по id
+# print(delete(country_api_server + '/2').json())  # подтверждение удаления
+# print(get(country_api_server).json())  # проверка результатов
+# """Некорректные"""
 """CountryListResource"""
-print(get(country_api_server).json())
-print(post(country_api_server, json={'id': 2, 'title': 'Италия'}).json())
-print(get(country_api_server).json())  # проверка добавления
-"""CountryResource"""
-print(get(country_api_server).json())  # проверка исходных значений
-print(get(country_api_server + '/2').json())  # получение страны по id
-print(put(country_api_server + '/2', json={'id': 2, 'title': 'США'}).json())  # изменение по id
-print(get(country_api_server + '/2').json())  # подтверждение изменения
-print(delete(country_api_server + '/2').json())  # удаление по id
-print(delete(country_api_server + '/2').json())  # подтверждение удаления
-print(get(country_api_server).json())  # проверка результатов
+# лишний слэш (/) API считает за пустой аргумент id -> неверно
+print(get('http://cloths-shop-prorotype.herokuapp.com/api/countries/').json())
+print(post(country_api_server, json={'title': 'Италия'}).json())  # пропущено поле id
+# id не может быть строкой - str. type(id) == int
+print(post(country_api_server, json={'id': 'abc', 'title': 'Италия'}).json())
---
+++ api_tests/test_order_api.py
@@ -0,0 +1,20 @@
+from requests import get, post, put, delete
+
+country_api_server = 'http://localhost:5000/api/orders'
+# 'id', 'items_id, is_finished, status' - возможные поля, используемые в бд
+"""Для выполнения корректного запроса требуется заполнение всех полей модели !"""
+"""
+Корректные
+"""
+"""CountryListResource"""
+print(get(country_api_server).json())
+print(post(country_api_server, json={'id': 2, 'title': 'Италия'}).json())
+print(get(country_api_server).json())  # проверка добавления
+"""CountryResource"""
+print(get(country_api_server).json())  # проверка исходных значений
+print(get(country_api_server + '/2').json())  # получение страны по id
+print(put(country_api_server + '/2', json={'id': 2, 'title': 'США'}).json())  # изменение по id
+print(get(country_api_server + '/2').json())  # подтверждение изменения
+print(delete(country_api_server + '/2').json())  # удаление по id
+print(delete(country_api_server + '/2').json())  # подтверждение удаления
+print(get(country_api_server).json())  # проверка результатов

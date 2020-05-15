37cfb98e36b8b35ae010ae52e17be574005bc72a
GlebVoronin <47082791+GlebVoronin@users.noreply.github.com>
2020-05-16 02:27:20 +0300

    Update test_usages_api_py.py

--- api_tests/test_usages_api_py.py
+++ api_tests/test_usages_api_py.py
@@ -1,6 +1,6 @@
 from requests import get, post, put, delete
 
-usage_api_server = 'http://cloths-shop-prorotype.herokuapp.com/api/usage'
+usage_api_server = 'http://cloths-shop-project.herokuapp.com/api/usage'
 # 'id', 'title' - возможные поля, используемые в бд
 """Для выполнения корректного запроса требуется заполнение всех полей модели кроме id!"""
 """Корректные"""

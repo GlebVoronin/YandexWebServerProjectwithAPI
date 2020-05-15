c41473065bf61994024da260db2b9a4ab8f26673
GlebVoronin <47082791+GlebVoronin@users.noreply.github.com>
2020-05-16 02:24:12 +0300

    Update test_cloth_api.py

--- api_tests/test_cloth_api.py
+++ api_tests/test_cloth_api.py
@@ -1,6 +1,6 @@
 from requests import get, post, put, delete
 
-cloth_api_server = 'http://cloths-shop-prorotype.herokuapp.com/api/cloths'
+cloth_api_server = 'http://cloths-shop-project.herokuapp.com/api/cloths'
 # 'id', 'title', 'description', 'images_links', 'colors',
 # 'length', 'price', 'date', 'country_id' - возможные поля, используемые в бд
 """Для выполнения корректного запроса требуется заполнение всех полей модели кроме id!"""
